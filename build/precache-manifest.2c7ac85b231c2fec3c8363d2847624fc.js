5826ee012496a437a0fe7fbe2a87e2f56116ffef
Raj Bhattacharya <rbhtta@amazon.com>
2019-06-11 10:33:39 +0530

    make it better

---
+++ build/precache-manifest.2c7ac85b231c2fec3c8363d2847624fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9645cf40a7731fa903b9ec39b23aff8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "362054390da18c01549c",
+    "url": "/static/css/main.c3e593cb.chunk.css"
+  },
+  {
+    "revision": "4288e78c71f68c6841f4",
+    "url": "/static/js/2.b9e398a3.chunk.js"
+  },
+  {
+    "revision": "362054390da18c01549c",
+    "url": "/static/js/main.a550382e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "58673c9eab5aca725f168b114c6648a1",
+    "url": "/static/media/like1.58673c9e.png"
+  }
+]);
